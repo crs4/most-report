def6cb47583ac7d8e66097a918ff69fc7524200c
Valeria Lecca <valeria.lecca@gmail.com>
2014-10-19 17:54:43 +0200

    node report api

--- src/most/report/report/views/__init__.py
+++ src/most/report/report/views/__init__.py
@@ -1,1 +1,5 @@
-__author__ = 'valeria'
+SUCCESS_KEY = 'success'
+MESSAGE_KEY = 'message'
+ERRORS_KEY = 'errors'
+DATA_KEY = 'data'
+TOTAL_KEY = 'total_count'
--- src/most/report/report/views/node_value.py
+++ src/most/report/report/views/node_value.py
@@ -1,1 +1,51 @@
-__author__ = 'valeria'
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST, require_GET
+from django.utils.translation import ugettext as _
+import json
+from datetime import date, datetime
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required, user_passes_test
+from most.web.exams.views import SUCCESS_KEY, MESSAGE_KEY, TOTAL_KEY, ERRORS_KEY, DATA_KEY
+from most.web.exams.forms import ExamForm
+
+@csrf_exempt
+@require_POST
+@login_required
+def new(request):
+    pass
+
+
+@csrf_exempt
+@require_POST
+@login_required
+def bulk_new(request):
+    pass
+
+
+@csrf_exempt
+@require_POST
+@login_required
+def edit(request, node_value_id):
+    pass
+
+
+@csrf_exempt
+@require_POST
+@login_required
+def bulk_edit(request):
+    pass
+
+
+@csrf_exempt
+@require_POST
+@login_required
+def delete(request, node_value_id):
+    pass
+
+
+@csrf_exempt
+@require_POST
+@login_required
+def bulk_delete(request):
+    pass
