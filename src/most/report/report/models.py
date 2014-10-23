b1d90b82b708dcda57808e810b05af3394e7e366
Valeria Lecca <valeria.lecca@gmail.com>
2014-10-23 15:50:39 +0200

    node report added

--- src/most/report/report/models.py
+++ src/most/report/report/models.py
@@ -75,32 +75,75 @@ class ExamNode(models.Model):
             sibling_list = None
         return sibling_list
 
+    def to_dictionary(self, exclude_children=False):
+        exam_node_dictionary = {
+            u'id': u'%s' % self.id,
+            u'section': u'%s' % self.section.to_dictionary(),
+            u'name': u'%s' % self.name,
+            u'root_node': u'%s' % self.root_node,
+            u'default_node': u'%s' % self.default_node,
+            u'order_number': u'%s' % self.order_number if self.order_number else None,
+            u'cached_path': u'%s' % self.cached_path if self.cached_path else None,
+            u'unique_node': u'%s' % self.unique_node
+        }
+        #children
+        if not exclude_children:
+            child_list = []
+            for child in exclude_children:
+                child_list.append(child.to_dictionary(exclude_children=True))
+        return exam_node_dictionary
+
 
 class NodeReport(models.Model):
-    exam_node = models.ForeignKey("ExamNode", verbose_name=_("Exam Node"), related_name="node_report", db_column="node_description", blank=True, null=True)
-    separator = models.CharField(_("Separator"), max_length=5, null=True, blank=True)
-    ignore_label = models.BooleanField(_("Ignore label"), default=False)
-
-    def __unicode__(self):
-        return self.exam_node
-
-
-class NodeValue(models.Model):
     VALUE_TYPE_CHOICES = (
         ('N', 'Number'),
         ('T', 'Text'),
         ('U', 'Undefined')
     )
 
-    exam_report = models.ForeignKey("NodeReport", null=True, blank=True, verbose_name=_("Node Report"), related_name="node_value", db_column="node_value")
-    value = models.CharField(_("Value"), max_length=500, null=True, blank=True)
+    exam_node = models.ForeignKey("ExamNode", verbose_name=_("Exam Node"), related_name="node_report", db_column="node_description", blank=True, null=True)
+    separator = models.CharField(_("Separator"), max_length=5, null=True, blank=True)
+    ignore_label = models.BooleanField(_("Ignore label"), default=False)
     unit_measure = models.CharField(_("Unit of measure"), max_length=50, null=True, blank=True)
     value_type = models.CharField(_("Value type"), choices=VALUE_TYPE_CHOICES, max_length=1, default='U')
 
     def __unicode__(self):
-        value_string = u'[%s] %s' % (self.value_type, self.exam_report)
+        return self.exam_node
+
+    def to_dictionary(self):
+        return {
+            u'id': u'%s' % self.id,
+            u'exam_node': u'%s' % self.exam_node.to_dictionary(exclude_children=True),
+            u'separator': u'%s' % self.separator if self.separator else None,
+            u'ignore_label': u'%s' % self.ignore_label,
+            u'unit_measure': u'%s' % self.unit_measure if self.unit_measure else None,
+            u'value_type': {
+                u'key': u'%s' % self.value_type,
+                u'value': u'%s' % self.get_value_type_display(),
+            },
+        }
+
+
+class NodeValue(models.Model):
+    clinician = models.ForeignKey(ClinicianUser, verbose_name='Clinician')
+    creation_datetime = models.DateTimeField(_('Creation DateTime'), auto_now_add=True)
+    modification_datetime = models.DateTimeField(_('Modification DateTime'), auto_now=True)
+    deletion_datetime = models.DateTimeField(_('Deletion Date Time'), null=True, blank=True)
+    node_report = models.ForeignKey("NodeReport", null=True, blank=True, verbose_name=_("Node Report"), related_name="node_value", db_column="node_value")
+    value = models.CharField(_("Value"), max_length=500, null=True, blank=True)
+
+    def __unicode__(self):
+        value_string = u'[%s] %s' % (self.node_report.value_type, self.node_report)
         if self.value:
             value_string = u'%s = %s' % (value_string, self.value)
-            if self.unit_measure:
-                value_string = u'%s = %s' % (value_string, self.unit_measure)
+            if self.node_report.unit_measure:
+                value_string = u'%s = %s' % (value_string, self.node_report.unit_measure)
         return value_string
+
+    def to_dictionary(self):
+        return {
+            u'id': u'%s' % self.id,
+            u'value': u'%s' % self.value if self.value else None,
+            u'clinician': u'%s' % self.clinician.to_dictionary(),
+            u'node_report': u'%s' % self.node_report.to_dictionary(),
+        }
--- src/most/report/report/views/node_value.py
+++ src/most/report/report/views/node_value.py
@@ -6,21 +6,105 @@ import json
 from datetime import date, datetime
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required, user_passes_test
-from most.web.exams.views import SUCCESS_KEY, MESSAGE_KEY, TOTAL_KEY, ERRORS_KEY, DATA_KEY
-from most.web.exams.forms import ExamForm
+from most.report.report.views import SUCCESS_KEY, MESSAGE_KEY, TOTAL_KEY, ERRORS_KEY, DATA_KEY
+from most.web.users.models import ClinicianUser
+from most.web.report.models import ExamNode, NodeReport, NodeValue
+from most.report.report.forms import ExamNodeForm
+
+
+def create_node_value(attributes):
+    try:
+        exam_node_value_form = ExamNodeForm(attributes)
+        if exam_node_value_form.is_valid():
+            exam_node_value = exam_node_value_form.save(commit=False)
+            return True, exam_node_value
+        else:
+            errors = ''
+            for field, error in exam_node_value_form.errors.items():
+                errors += '\n%s\n' % error
+            return False, errors
+    except Exception, e:
+        return False, e
+
+
+def bulk_create_node_value(nodes_attributes):
+    passed_nodes = []
+    errors = ''
+    for node_attributes in nodes_attributes:
+        try:
+            passed, node_instance = create_node_value(node_attributes)
+            if passed:
+                passed_nodes.append(node_instance)
+            else:
+                errors += '%s\n' % node_instance
+        except Exception, e:
+            errors += '%s\n' % e
+    return passed_nodes if passed_nodes else None, errors if errors else None
+
+
+def edit_node_value(node_id, attributes):
+    try:
+        exam_node_value = NodeValue.objects.get(pk=node_id)
+        exam_node_value_form = ExamNodeForm(attributes, instance=exam_node_value)
+        if exam_node_value_form.is_valid():
+            exam_node_value = exam_node_value_form.save()
+            return True, exam_node_value
+        else:
+            errors = ''
+            for field, error in exam_node_value_form.errors.items():
+                errors += '\n%s\n' % error
+            return False, errors
+    except Exception, e:
+        return False, e
+
+
+def delete_node_value(node_id):
+    try:
+        node_value = NodeValue.objects.filter(id=node_id).delete()
+        return True, node_value
+    except Exception, e:
+        return False, e
+
 
 @csrf_exempt
 @require_POST
 @login_required
 def new(request):
-    pass
+    results = {}
+    try:
+        results[SUCCESS_KEY], exam_node_value = create_node_value(request.POST)
+        if results[SUCCESS_KEY]:
+            exam_node_value.save()
+            results[MESSAGE_KEY] = _('Node value %s successfully created.' % exam_node_value.pk)
+            results[DATA_KEY] = exam_node_value.to_dictionary()
+        else:
+            results[ERRORS_KEY] = _('Unable to create section. %s' % exam_node_value)
+    except Exception, e:
+        results[SUCCESS_KEY] = False
+        results[ERRORS_KEY] = e
+    return HttpResponse(json.dumps(results), content_type='application/json; charset=utf8')
 
 
 @csrf_exempt
 @require_POST
 @login_required
 def bulk_new(request):
-    pass
+    results = {}
+    try:
+        passed_nodes, results[ERRORS_KEY] = bulk_create_node_value(request.POST)
+        if not results[ERRORS_KEY]:
+            for node in passed_nodes:
+                node.save()
+            results[SUCCESS_KEY] = True
+            results[MESSAGE_KEY] = _('Node value %s successfully created.' % exam_node_value.pk)
+            results[DATA_KEY] = exam_node_value.to_dictionary()
+        else:
+            results[ERRORS_KEY] = _('Unable to create section. %s' % exam_node_value)
+    except Exception, e:
+        results[SUCCESS_KEY] = False
+        results[ERRORS_KEY] = e
+    return HttpResponse(json.dumps(results), content_type='application/json; charset=utf8')
+
 
 
 @csrf_exempt
